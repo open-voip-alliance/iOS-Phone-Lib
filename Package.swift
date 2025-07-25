bbf29ee1be6393c1a5c5be355bfa6f35790c834a
Johannes Nevels <johannes.nevels@voys.nl>
2025-07-25 10:18:51 +0200

    fix: remove unsupported local package

--- Package.swift
+++ Package.swift
@@ -1,5 +1,4 @@
 // swift-tools-version:5.9
-
 import PackageDescription
 
 let package = Package(
@@ -15,14 +14,21 @@ let package = Package(
     ],
     dependencies: [
         .package(url: "https://github.com/JohannesNevels/Swinject.git", branch: "master"),
-        .package(path: "../LinphoneWrapper")
+        .package(url: "https://gitlab.linphone.org/BC/public/linphone-sdk-swift-ios.git", .exact("5.4.24"))
     ],
     targets: [
+        .target(
+            name: "LinphoneWrapper",
+            dependencies: [
+                .product(name: "linphonesw", package: "linphone-sdk-swift-ios")
+            ],
+            path: "LinphoneWrapper/Sources"
+        ),
         .target(
             name: "iOSPhoneLib",
             dependencies: [
                 .product(name: "Swinject", package: "Swinject"),
-                .product(name: "LinphoneWrapper", package: "LinphoneWrapper")
+                "LinphoneWrapper"
             ],
             path: "iOSPhoneLib",
             resources: [
